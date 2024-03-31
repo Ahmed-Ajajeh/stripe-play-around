499ec06f67f846fbc7e8bb66e0e09e5a1abc9fa9
Ahmed-Ajajeh <ajajeh356@gmail.com>
2024-03-31 16:08:45 +0300

    updated

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ app.use(userRouter);
 app.listen(3000);
 
 // connecting to db :
-mongoose.connect("mongodb://localhost:27017").then((connection) => {
-  // console.log(connection);
-  console.log("DATABASE CONNECTED");
-});
+mongoose
+  .connect(
+    "mongodb+srv://ajajeh356:testingtesting@project.hnzeyqm.mongodb.net/?retryWrites=true&w=majority&appName=project"
+  )
+  .then((connection) => {
+    console.log(connection);
+    console.log("DATABASE CONNECTED");
+  });
